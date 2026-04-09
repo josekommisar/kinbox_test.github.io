836c6caa81cdcae6ed8b923bac6f3cb6a35149da
Jose Kommisar <136255742+josekommisar@users.noreply.github.com>
2026-04-09 04:21:54 -0300

    Modify console logs in plugin.js for clarity
    Updated console log messages for debugging purposes.

--- plugin.js
+++ plugin.js
@@ -3,6 +3,7 @@
  ********************/
 var conversation
 var workspaceInfo
+var payload
 
 /********************
  * Eventos
@@ -10,13 +11,14 @@ var workspaceInfo
 
 Kinbox.on("conversation", function (data) {
     conversation = data
-    console.log("on conversation", data)
+    console.log("on conversation1", data)
 
     getWorkspaceInfo()
 })
 
 Kinbox.getWorkspaceInfo(function (payload) {
-    console.log("the payload", payload)
+    payload = payload
+    console.log("the payload1", payload)
 })
 
 /********************
@@ -26,7 +28,7 @@ Kinbox.getWorkspaceInfo(function (payload) {
 function getWorkspaceInfo() {
     // Obter informações do workspace
     Kinbox.getWorkspaceInfo(async function (data) {
-        console.log("on workspaceInfo", data)
+        console.log("on workspaceInfo1", data)
         workspaceInfo = data
     })
 }
@@ -34,7 +36,7 @@ function getWorkspaceInfo() {
 function getWorkspaceInfo() {
     // Obter informações do workspace
     Kinbox.getWorkspaceInfo(async function (payload) {
-        console.log("the payload", payload)
+        console.log("the payload2", payload)
         workspaceInfo = payload
     })
 }
